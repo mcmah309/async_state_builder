faba9009b7b93fca235704f068712dc0f849efcc
mcmah309 <dillonmcmahon79@gmail.com>
2024-09-18 01:54:00 +0000

    feat: Comment out extension

--- example/lib/extension.dart
+++ example/lib/extension.dart
@@ -1,110 +1,110 @@
-import 'dart:async';
-import 'package:async_state_builder/async_state_builder.dart';
-import 'package:flutter/material.dart';
+// import 'dart:async';
+// import 'package:async_state_builder/async_state_builder.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CounterPage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: CounterPage(),
+//     );
+//   }
+// }
 
-class CounterPage extends StatefulWidget {
-  const CounterPage({super.key});
+// class CounterPage extends StatefulWidget {
+//   const CounterPage({super.key});
 
-  @override
-  CounterPageState createState() => CounterPageState();
-}
+//   @override
+//   CounterPageState createState() => CounterPageState();
+// }
 
-class CounterPageState extends State<CounterPage> {
-  StreamController<int> _counterController = StreamController<int>();
-  int _counter = 0;
-  bool _hasWaitedTooLong = false;
+// class CounterPageState extends State<CounterPage> {
+//   StreamController<int> _counterController = StreamController<int>();
+//   int _counter = 0;
+//   bool _hasWaitedTooLong = false;
 
-  @override
-  void initState() {
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
 
-  void _incrementCounter() {
-    _counter++;
-    _counterController.add(_counter);
-  }
+//   void _incrementCounter() {
+//     _counter++;
+//     _counterController.add(_counter);
+//   }
 
-  void _closeStream() {
-    _counterController.close();
-  }
+//   void _closeStream() {
+//     _counterController.close();
+//   }
 
-  void _sendError() {
-    _counterController.addError('An error occurred!');
-  }
+//   void _sendError() {
+//     _counterController.addError('An error occurred!');
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('StreamStateBuilder example'),
-      ),
-      body: _hasWaitedTooLong
-          ? const Center(child: Text("Waited too long, callback invoked"))
-          : switch (context.watchStream(_counterController.stream)) {
-              Waiting() => const Text('Waiting for data...'),
-              StreamError<int>(:final error) =>
-                Text('Error: $error'),
-              Data<int>(:final data) => Text('Data sent without error: $data'),
-            },
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                _counterController.close();
-                _counterController = StreamController();
-                _counter = 0;
-                _hasWaitedTooLong = false;
-              });
-            },
-            tooltip: 'Restart',
-            child: const Icon(Icons.refresh),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {});
-            },
-            tooltip: 'Trigger rebuild',
-            child: const Icon(Icons.build),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Send Data',
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _sendError,
-            tooltip: 'Send Error',
-            child: const Icon(Icons.error),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _closeStream,
-            tooltip: 'Close Stream',
-            backgroundColor: Colors.red,
-            child: const Icon(Icons.close),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('StreamStateBuilder example'),
+//       ),
+//       body: _hasWaitedTooLong
+//           ? const Center(child: Text("Waited too long, callback invoked"))
+//           : switch (context.watchStream(_counterController.stream)) {
+//               Waiting() => const Text('Waiting for data...'),
+//               StreamError<int>(:final error) =>
+//                 Text('Error: $error'),
+//               Data<int>(:final data) => Text('Data sent without error: $data'),
+//             },
+//       floatingActionButton: Column(
+//         mainAxisAlignment: MainAxisAlignment.end,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: () {
+//               setState(() {
+//                 _counterController.close();
+//                 _counterController = StreamController();
+//                 _counter = 0;
+//                 _hasWaitedTooLong = false;
+//               });
+//             },
+//             tooltip: 'Restart',
+//             child: const Icon(Icons.refresh),
+//           ),
+//           const SizedBox(height: 10),
+//           FloatingActionButton(
+//             onPressed: () {
+//               setState(() {});
+//             },
+//             tooltip: 'Trigger rebuild',
+//             child: const Icon(Icons.build),
+//           ),
+//           const SizedBox(height: 10),
+//           FloatingActionButton(
+//             onPressed: _incrementCounter,
+//             tooltip: 'Send Data',
+//             child: const Icon(Icons.add),
+//           ),
+//           const SizedBox(height: 10),
+//           FloatingActionButton(
+//             onPressed: _sendError,
+//             tooltip: 'Send Error',
+//             child: const Icon(Icons.error),
+//           ),
+//           const SizedBox(height: 10),
+//           FloatingActionButton(
+//             onPressed: _closeStream,
+//             tooltip: 'Close Stream',
+//             backgroundColor: Colors.red,
+//             child: const Icon(Icons.close),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -1,79 +1,79 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:async_state_builder/src/common.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:async_state_builder/src/common.dart';
+// import 'package:flutter/widgets.dart';
 
-class _StreamWatcherContainer {
-  static final Map<Stream, _StreamWidgetSubscription> map = {};
-}
+// class _StreamWatcherContainer {
+//   static final Map<Stream, _StreamWidgetSubscription> map = {};
+// }
 
-class _StreamWidgetSubscription<T> {
-  final Stream<T> stream;
-  late final StreamSubscription<T> subscription;
-  StreamState<T> lastValue;
-  Set<Element> watchers = {};
+// class _StreamWidgetSubscription<T> {
+//   final Stream<T> stream;
+//   late final StreamSubscription<T> subscription;
+//   StreamState<T> lastValue;
+//   Set<Element> watchers = {};
 
-  _StreamWidgetSubscription(this.stream, this.lastValue, Element initialWatcher) {
-    watchers.add(initialWatcher);
-  }
+//   _StreamWidgetSubscription(this.stream, this.lastValue, Element initialWatcher) {
+//     watchers.add(initialWatcher);
+//   }
 
-  void removeWatcher(Element watcher) {
-    watchers.remove(watcher);
-    if (watchers.isNotEmpty) {
-      return;
-    }
-    subscription.cancel();
-    final removed = _StreamWatcherContainer.map.remove(stream);
-    assert(removed == this);
-  }
+//   void removeWatcher(Element watcher) {
+//     watchers.remove(watcher);
+//     if (watchers.isNotEmpty) {
+//       return;
+//     }
+//     subscription.cancel();
+//     final removed = _StreamWatcherContainer.map.remove(stream);
+//     assert(removed == this);
+//   }
 
-  @override
-  bool operator ==(Object other) {
-    return other is _StreamWidgetSubscription<T> && stream == other.stream;
-  }
+//   @override
+//   bool operator ==(Object other) {
+//     return other is _StreamWidgetSubscription<T> && stream == other.stream;
+//   }
 
-  @override
-  int get hashCode => stream.hashCode;
-}
+//   @override
+//   int get hashCode => stream.hashCode;
+// }
 
-extension BuildContextExt on BuildContext {
-  /// Watches a stream, rebuilding the current widget each time the stream updates.
-  /// Remember to close the [stream] when the widget is disposed.
-  StreamState<T> watchStream<T>(Stream<T> stream, {T? initialValue}) {
-    _StreamWidgetSubscription<T>? streamWidgetSubscription =
-        _StreamWatcherContainer.map[stream] as _StreamWidgetSubscription<T>?;
-    final StreamState<T> state;
-    if (streamWidgetSubscription == null) {
-      final element = this as Element;
-      if (initialValue == null) {
-        state = const Waiting();
-      } else {
-        state = Data(initialValue);
-      }
-      streamWidgetSubscription = _StreamWidgetSubscription(stream, state, element);
-      _StreamWatcherContainer.map[stream] = streamWidgetSubscription;
-      final subscription = stream.listen((data) {
-        if (element.mounted) {
-          element.markNeedsBuild();
-          streamWidgetSubscription!.lastValue = Data(data);
-        } else {
-          _StreamWatcherContainer.map[stream]!.removeWatcher(element);
-        }
-      }, onDone: () {
-        _StreamWatcherContainer.map[stream]!.removeWatcher(element);
-      }, onError: (Object error, StackTrace stackTrace) {
-        if (element.mounted) {
-          element.markNeedsBuild();
-          streamWidgetSubscription!.lastValue = StreamError(error, stackTrace);
-        } else {
-          _StreamWatcherContainer.map[stream]!.removeWatcher(element);
-        }
-      });
-      // An implementation like a sync stream may have already ran the above, so this added later.
-      streamWidgetSubscription.subscription = subscription;
-    } else {
-      state = streamWidgetSubscription.lastValue;
-    }
-    return state;
-  }
-}
+// extension BuildContextExt on BuildContext {
+//   /// Watches a stream, rebuilding the current widget each time the stream updates.
+//   /// Remember to close the [stream] when the widget is disposed.
+//   StreamState<T> watchStream<T>(Stream<T> stream, {T? initialValue}) {
+//     _StreamWidgetSubscription<T>? streamWidgetSubscription =
+//         _StreamWatcherContainer.map[stream] as _StreamWidgetSubscription<T>?;
+//     final StreamState<T> state;
+//     if (streamWidgetSubscription == null) {
+//       final element = this as Element;
+//       if (initialValue == null) {
+//         state = const Waiting();
+//       } else {
+//         state = Data(initialValue);
+//       }
+//       streamWidgetSubscription = _StreamWidgetSubscription(stream, state, element);
+//       _StreamWatcherContainer.map[stream] = streamWidgetSubscription;
+//       final subscription = stream.listen((data) {
+//         if (element.mounted) {
+//           element.markNeedsBuild();
+//           streamWidgetSubscription!.lastValue = Data(data);
+//         } else {
+//           _StreamWatcherContainer.map[stream]!.removeWatcher(element);
+//         }
+//       }, onDone: () {
+//         _StreamWatcherContainer.map[stream]!.removeWatcher(element);
+//       }, onError: (Object error, StackTrace stackTrace) {
+//         if (element.mounted) {
+//           element.markNeedsBuild();
+//           streamWidgetSubscription!.lastValue = StreamError(error, stackTrace);
+//         } else {
+//           _StreamWatcherContainer.map[stream]!.removeWatcher(element);
+//         }
+//       });
+//       // An implementation like a sync stream may have already ran the above, so this added later.
+//       streamWidgetSubscription.subscription = subscription;
+//     } else {
+//       state = streamWidgetSubscription.lastValue;
+//     }
+//     return state;
+//   }
+// }
